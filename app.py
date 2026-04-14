8e0fc77e2698cd22c2aa5bb57e4b2d423c5456dc
rahalimoha-png <rahali.moha@icloud.com>
2026-04-14 03:02:41 +0100

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import os
 
 app = Flask(__name__)
 
-API_KEY = os.getenv("08c67418b0411043bc7dab3af28068c8", "")
+API_KEY = os.environ.get("08c67418b0411043bc7dab3af28068c8")
 
 URL = "https://v3.football.api-sports.io/fixtures?date=today"
 
@@ -12,18 +12,22 @@ headers = {
     "x-apisports-key": API_KEY
 }
 
+@app.route("/", methods=["GET"])
+def home():
+    return "Server is running ✅"
+
 @app.route("/today", methods=["GET"])
 def today():
 
-    if API_KEY == "":
-        return jsonify({"error": "API KEY missing"})
+    if not API_KEY:
+        return jsonify({"error": "API_KEY not set in Environment Variables"})
 
     try:
         res = requests.get(URL, headers=headers).json()
 
         matches = []
 
-        for m in res["response"][:5]:
+        for m in res.get("response", [])[:5]:
 
             home = m["teams"]["home"]["name"]
             away = m["teams"]["away"]["name"]
