2330df5e2adb20a2648707997191310cd4602aca
rahalimoha-png <rahali.moha@icloud.com>
2026-04-14 02:49:39 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,58 +1,22 @@
 from flask import Flask, jsonify
 import requests
-import random
+import os
 
 app = Flask(__name__)
 
-API_KEY = "08c67418b0411043bc7dab3af28068c8"
+API_KEY = os.getenv("08c67418b0411043bc7dab3af28068c8", "")
+
 URL = "https://v3.football.api-sports.io/fixtures?date=today"
 
 headers = {
     "x-apisports-key": API_KEY
 }
 
-def analyze(home, away):
-
-    # ⚽ قوة ذكية
-    home_power = random.uniform(70, 95)
-    away_power = random.uniform(65, 90)
-
-    home_adv = 6
-
-    home_score = home_power + home_adv
-    away_score = away_power
-
-    total = home_score + away_score
-
-    home_win = round((home_score / total) * 100, 1)
-    away_win = round((away_score / total) * 100, 1)
-
-    diff = abs(home_win - away_win)
-
-    if home_win > 58:
-        pick = home
-    elif away_win > 58:
-        pick = away
-    else:
-        pick = "No strong pick"
-
-    if diff > 20:
-        confidence = "🎯 آمن"
-    elif diff > 10:
-        confidence = "⚠️ متوسط"
-    else:
-        confidence = "💣 خطر"
-
-    return {
-        "match": f"{home} vs {away}",
-        "pick": pick,
-        "home_win_%": home_win,
-        "away_win_%": away_win,
-        "confidence": confidence
-    }
-
 @app.route("/today", methods=["GET"])
 def today():
+
+    if API_KEY == "":
+        return jsonify({"error": "API KEY missing"})
 
     try:
         res = requests.get(URL, headers=headers).json()
@@ -64,18 +28,14 @@ def today():
             home = m["teams"]["home"]["name"]
             away = m["teams"]["away"]["name"]
 
-            matches.append(analyze(home, away))
+            matches.append({
+                "match": f"{home} vs {away}"
+            })
 
-        # 🔥 اختيار أفضل مباراة
-        best = max(matches, key=lambda x: abs(x["home_win_%"] - x["away_win_%"]))
+        return jsonify(matches)
 
-        return jsonify({
-            "matches": matches,
-            "best_pick": best
-        })
-
-    except:
-        return jsonify({"error": "API failed"})
+    except Exception as e:
+        return jsonify({"error": str(e)})
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=10000)
